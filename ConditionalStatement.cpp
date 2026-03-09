63014b0f680aa50c4ddabedbc73b71a25c2aaac2
fadlan243 <fadlan.nasrizal17@sma.belajar.id>
2026-03-09 20:14:38 +0700

    fungsi menentukan status berat badan

--- ConditionalStatement.cpp
+++ ConditionalStatement.cpp
@@ -16,3 +16,19 @@ float hitungBMI(float berat, float tinggi){
     bmi = berat / (tinggi * tinggi);
     return bmi;
 }
+
+// Fungsi untuk menentukan status berat badan
+string statusBMI(float bmi){
+    if(bmi < 18.5){
+        return "Berat Badan Kurang";
+    }
+    else if(bmi < 25){
+        return "Berat Badan Normal";
+    }
+    else if(bmi < 30){
+        return "Berat Badan Kelebihan";
+    }
+    else{
+        return "Obesitas";
+    }
+}
